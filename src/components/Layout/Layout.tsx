9421f8d73413b5e6edb958e2b64693f2a6748de3
Vanesflat <ptptcptptc@yandex.ru>
2023-04-03 12:59:30 +0700

    Connect react-helmet-async and add layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,23 @@
+import { Helmet } from 'react-helmet-async';
+import Footer from '../Footer/Footer';
+import Header from '../Header/Header';
+
+type LayoutProps = {
+  pageTitle?: string;
+  children: React.ReactNode;
+}
+
+function Layout({ pageTitle, children }: LayoutProps): JSX.Element {
+  return (
+    <div className="wrapper">
+      <Helmet>
+        <title>Sultan Shop {pageTitle}</title>
+      </Helmet>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
