826ee6d41d180eff3c0faef422c5b41ccccf7346
Vanesflat <ptptcptptc@yandex.ru>
2023-04-04 13:59:20 +0700

    Minor update

--- src/hooks/store.ts
+++ src/hooks/store.ts
@@ -1,5 +1,5 @@
-import { AppDispatch, State } from '../types/state';
 import { TypedUseSelectorHook, useSelector, useDispatch } from 'react-redux';
+import { AppDispatch, RootState } from '../types/state';
 
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppDispatch = () => useDispatch<AppDispatch>();
--- src/types/state.ts
+++ src/types/state.ts
@@ -1,10 +1,10 @@
-import { store } from '../store/store';
+import { setupStore, rootReducer } from '../store/store';
 import { Product } from './product';
 
 export type InitialState = {
   products: Product[];
 };
 
-export type State = ReturnType<typeof store.getState>;
-
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore['dispatch'];
