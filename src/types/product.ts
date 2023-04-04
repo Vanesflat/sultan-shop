2f87a779ffd6faaffb6b08981c01baab4cb0aa47
Vanesflat <ptptcptptc@yandex.ru>
2023-04-04 12:54:30 +0700

    Add type for product

---
+++ src/types/product.ts
@@ -0,0 +1,14 @@
+export type Product = {
+  id: string,
+  imgUrl: string,
+  name: string,
+  sizeType: 'weight' | 'volume',
+  size: string,
+  barcode: string,
+  producer: string,
+  brand: string,
+  description: string,
+  price: string,
+  inStock: boolean,
+  types: string[]
+};
