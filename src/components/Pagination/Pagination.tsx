737f1e1c11d8a49fbab79696265d74b77a6abbce
Vanesflat <ptptcptptc@yandex.ru>
2023-05-05 18:22:34 +0700

    Connect react-paginate. Implement pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,22 +1,29 @@
-function Pagination(): JSX.Element {
+import ReactPaginate from 'react-paginate';
+
+type PaginationProps = {
+  pageCount: number;
+  onChange: React.Dispatch<React.SetStateAction<number>>;
+};
+
+function Pagination({ pageCount, onChange }: PaginationProps): JSX.Element {
   return (
-    <div className="catalog__pagination pagination">
-      <button className="pagination__btn pagination__btn--prev">
-        <img src="images/catalog/arrow-prev.svg" alt="Предыдущая страница" />
-      </button>
-
-      <ul className="pagination__list">
-        <li className="pagination__item pagination__item--active"><a className="pagination__link" href="/#">1</a></li>
-        <li className="pagination__item"><a className="pagination__link" href="/#">2</a></li>
-        <li className="pagination__item"><a className="pagination__link" href="/#">3</a></li>
-        <li className="pagination__item"><a className="pagination__link" href="/#">4</a></li>
-        <li className="pagination__item"><a className="pagination__link" href="/#">5</a></li>
-      </ul>
-
-      <button className="pagination__btn pagination__btn--next">
-        <img src="images/catalog/arrow-next.svg" alt="Следующая страница" />
-      </button>
-    </div>
+    <ReactPaginate
+      className='catalog__pagination pagination'
+      previousLabel={
+        <button className="pagination__btn pagination__btn--prev">
+          <img src="images/catalog/arrow-prev.svg" alt="Предыдущая страница" />
+        </button>}
+      nextLabel={
+        <button className="pagination__btn pagination__btn--next">
+          <img src="images/catalog/arrow-next.svg" alt="Следующая страница" />
+        </button>}
+      previousClassName='pagination__btn pagination__btn--prev'
+      nextClassName='pagination__btn pagination__btn--next'
+      pageClassName='pagination__item'
+      activeClassName='pagination__item--active'
+      onPageChange={(event) => onChange(event.selected + 1)}
+      pageCount={pageCount}
+    />
   );
 }
 
--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -9,6 +9,9 @@ import { getSortedProducts } from '../utils/sort';
 import Categories from '../components/Categories/Categories';
 import { getFilteredProducts } from '../utils/filter';
 import { getCurrentCategory, getCurrentSortType } from '../store/reducers/Products/selectors';
+import { useState } from 'react';
+
+const PRODUCTS_PER_PAGE = 9;
 
 type CatalogPageProps = {
   products: Product[];
@@ -19,6 +22,11 @@ function CatalogPage({ products }: CatalogPageProps): JSX.Element {
   const category = useAppSelector(getCurrentCategory);
   const sortedProducts = getSortedProducts(products, sortType);
   const filteredProducts = getFilteredProducts(sortedProducts, category);
+  const [currentPage, setCurrentPage] = useState(1);
+  const pageCount = Math.ceil(filteredProducts.length / PRODUCTS_PER_PAGE);
+  const renderedProducts = filteredProducts.slice((currentPage - 1) * PRODUCTS_PER_PAGE, currentPage * PRODUCTS_PER_PAGE);
+
+  console.log(currentPage);
 
   return (
     <Layout pageTitle="Каталог">
@@ -36,9 +44,9 @@ function CatalogPage({ products }: CatalogPageProps): JSX.Element {
 
               <Sort sortType={sortType} />
 
-              <ProductList classNames="catalog" products={filteredProducts} />
+              <ProductList classNames="catalog" products={renderedProducts} />
 
-              <Pagination />
+              {pageCount > 1 && <Pagination pageCount={pageCount} onChange={setCurrentPage} />}
 
               <p className="catalog__info">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam interdum ut
