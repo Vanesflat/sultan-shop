8b54e91d2fa5f3af3a32771775add0b16a39b569
Vanesflat <ptptcptptc@yandex.ru>
2023-05-05 15:36:53 +0700

    Add products empty component and implement display logic

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -2,6 +2,7 @@ import { useAppSelector } from '../../hooks/store';
 import { getFetchStatus } from '../../store/reducers/Products/selectors';
 import { Product } from '../../types/product';
 import ProductCard from '../ProductCard/ProductCard';
+import ProductsEmpty from '../ProductsEmpty/ProductsEmpty';
 import Skeleton from '../Skeleton/Skeleton';
 
 type ProductListProps = {
@@ -11,6 +12,10 @@ type ProductListProps = {
 
 function ProductList({ classNames, products }: ProductListProps): JSX.Element {
   const fetchStatus = useAppSelector(getFetchStatus);
+
+  if (!products.length) {
+    return <ProductsEmpty />;
+  }
 
   return (
     <div className={`${classNames}__product-list product__list`}>
